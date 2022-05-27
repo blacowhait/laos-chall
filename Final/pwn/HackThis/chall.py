0ae9d5940f54075eacc731a88bac1303bb1a0af5
Linuz Tri Erianto <linuztri@gmail.com>
2022-05-27 23:18:19 +0700

    add pwn

--- Final/pwn/HackThis/chall.py
+++ Final/pwn/HackThis/chall.py
@@ -1,3 +1,24 @@
+#!/usr/bin/python3.8
+
+import time
+import sys
+import os
+
+class Unbuffered(object):
+  def __init__(self, stream):
+    self.stream = stream
+  def write(self, data):
+    self.stream.write(data)
+    self.stream.flush()
+  def writelines(self, datas):
+    self.stream.writelines(datas)
+    self.stream.flush()
+  def __getattr__(self, attr):
+    return getattr(self.stream, attr)
+
+sys.stdout = Unbuffered(sys.stdout)
+
+
 print("## CAN YOU HACK THIS")
 x = input(">>> ")
 print(eval(x))
